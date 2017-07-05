0c33fd83f9846b0cf1a31f3c2d48bcd03ac8f0d6
SamTrinidad <samtrinidad@gmail.com>
2017-07-05 11:48:21 -0500

    edited gitignore

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,5 @@
 
 $(document).ready(function(){
-    // $.get('../app/templates/init.php', function(data) {
-    //     $('#container').html(data);      
-    // });
-    // //iterate through all <a> tags and address them to the proper controller
-
-    // $('nav').children().each(function( index ) {
-    //     $(this).click(function() {
-
-    //         $('.activelink').removeClass('activelink');
-    //         $(this).addClass('activelink');
-
-    //         var links = ['','about','music','shows','blog','contact'];
-            
-    //         //use get request to fill container with the content
-    //         $.get('/app/templates/mvc.php?link='+links[index], function(data) {
-    //             $('#container').html(data);
-    //         }).fail(function(){
-    //             $('#container').html("Failed to load Content");
-    //         }); 
-    //         navSwitch();
-    //     });
-    // });
 
     //event of clicking the nav burger
     $('#burgercontainer').click(function() {
