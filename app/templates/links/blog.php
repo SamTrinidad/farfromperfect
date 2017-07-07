f42be922aa8311f41e4e3a8780a47e189db447c2
SamTrinidad <samtrinidad@gmail.com>
2017-07-07 15:21:30 -0500

    added comments

--- app/templates/links/blog.php
+++ app/templates/links/blog.php
@@ -6,7 +6,7 @@ class Blog extends Controller{
         $this->view('blog/index');
     }
 
-    public function login($logincode = '', $connection = ''){
+    public function login($logincode = '', $connection = ''){ //check for parameters at login
         if($logincode == 'THIQQSUCC' && $connection == 'connect'){
             $uname = $pwd = '';
 
@@ -17,37 +17,41 @@ class Blog extends Controller{
                 $uname = $this->test_input($_POST['uname']);
                 $pwd = $this->test_input($_POST['pwd']);
 
-                include $_SERVER['DOCUMENT_ROOT'] . '/farfromperfect/app/db/dbConnect.php';
+                include $_SERVER['DOCUMENT_ROOT'] . '/farfromperfect/app/db/dbConnect.php'; //include the db credentials
 
-                $db = new dbConnect();
+                $db = new dbConnect(); //create a new object of the connection
 
-                $connection = $db->getConnection();
+                $connection = $db->getConnection(); //get the connection instance
 
                 if($connection){
+
+                    //real escape the strings and strip slashes
                     $uname = $db->clean($uname);
                     $pwd = $db->clean($pwd);
 
-                    $query = $connection->query("select * from members where password='$pwd' AND username='$uname'");
+                    $query = $connection->query("select * from members where password='$pwd' AND username='$uname'"); //query for login
 
                     $rows = mysqli_num_rows($query);
 
                     if($rows == 1){
-                        echo "Login success, Welcome ";
-                        $this->view('blog/connect', ['uname' => $uname]);
+                        $this->view('blog/connect', ['uname' => $uname]); //pass view with user
                         return;
                     }
+                    $query->close();
+                    $db->closeConnection();
                 }       
             }
 
-            $this->view('blog/badlogin');
+            $this->view('blog/badlogin'); //if anything fails view bad login
             
-        }else if($logincode == 'THIQQSUCC'){
+        }else if($logincode == 'THIQQSUCC'){ //go to login page if no other parameters
             $this->view('blog/login');
         }else{
-            $this->index();
+            $this->index(); //go to blog page by default
         }
     }
 
+//testing the input so there isnt any injections
     private function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
